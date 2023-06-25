d4945f6488c26739df5579dafb0ca8527157827b
Araf Karsh Hamid <araf.karsh@metamagic.in>
2023-06-25 18:57:57 +0530

    Added Kafka Connect Example for PostgreSQL with Debezium

---
+++ src/main/java/io/fusion/air/microservice/adapters/cdc/KafkaCDCPostgreSQLService.java
@@ -0,0 +1,142 @@
+/**
+ * (C) Copyright 2023 Araf Karsh Hamid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fusion.air.microservice.adapters.cdc;
+
+import io.fusion.air.microservice.domain.exceptions.MessagingException;
+import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * CDC - Change Data Capture
+ * Kafka Connect Example
+ *
+ * For PostgreSQL
+ *
+ * 1. Download the Driver for Kafka Connect
+ *    https://debezium.io/documentation/reference/stable/install.html
+ *
+ * 2. config/connect-distributed.properties (Where Kafka is installed)
+ *    Add plugin.path=/kafka/connect
+ *
+ * 3. Update postgresql.conf with the following params
+ *    ############ REPLICATION ##############
+ *    wal_level = logical
+ *    max_wal_senders = 4
+ *    max_replication_slots = 4
+ *
+ * 4. Update pg_hba.conf with the following (if not already present)
+ *    ############ REPLICATION ##############
+ *    local   replication     postgres                          trust
+ *    host    replication     postgres  127.0.0.1/32            trust
+ *    host    replication     postgres  ::1/128                 trust
+ *
+ * 5. CREATE ROLE
+ *    CREATE ROLE name REPLICATION LOGIN;
+ *
+ * @author: Araf Karsh Hamid
+ * @version:
+ * @date:
+ */
+@Service
+public class KafkaCDCPostgreSQLService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    /**
+     * List All the Connectors
+     * @return
+     */
+    public String[] listAllConnectors() {
+        String uri = "http://localhost:8083/connectors";
+        String[] connectors = restTemplate.getForObject(uri, String[].class);
+        System.out.println(Arrays.toString(connectors));
+        return connectors;
+    }
+
+
+    /**
+     * Delete the Connector
+     * @param _connectorName
+     * @return
+     */
+    public String deleteConnector(String _connectorName) {
+        String uri = "http://localhost:8083/connectors/"+_connectorName;
+        String status = "Connector ("+_connectorName+") deleted successfully";
+        try {
+            restTemplate.delete(uri);
+            System.out.println(status);
+            return status;
+        } catch (HttpClientErrorException | ResourceAccessException e) {
+            status = "Error deleting connector ("+_connectorName+"): " + e.getMessage();
+            System.out.println(status);
+            throw new MessagingException(status);
+        }
+    }
+
+    /**
+     * Create the Connector
+     * @return
+     */
+    public String createPostgresConnectorForProduct() {
+        String kafkaConnectUri = "http://localhost:8083/connectors"; // replace with your Kafka Connect URI
+
+        Map<String, Object> config = new HashMap<>();
+        config.put("connector.class", "io.debezium.connector.postgresql.PostgresConnector");
+        config.put("database.hostname", "localhost");               // replace with your DB hostname
+        config.put("database.port", "5433");                        // replace with your DB port
+        config.put("database.user", "postgres");                    // replace with your DB user
+        config.put("database.password", "");                        // replace with your DB password
+        config.put("database.dbname", "ms_cache");                  // replace with your DB name
+        config.put("database.server.name", "ms_cache");
+        config.put("table.include.list", "public.products_m");
+        config.put("topic.creation.default.replication.factor", "1");
+        config.put("topic.creation.default.partitions", "1");
+        config.put("slot.name", "debezium_slot");
+        config.put("plugin.name", "pgoutput");
+        config.put("snapshot.mode", "initial");
+        config.put("decimal.handling.mode", "string");
+        config.put("topic.prefix", "ms_cache");                       // adding the topic.prefix value
+        config.put("transforms", "unwrap");
+        config.put("transforms.unwrap.type", "io.debezium.transforms.ExtractNewRecordState");
+        config.put("transforms.unwrap.drop.tombstones", "false");
+
+        Map<String, Object> connectorPayload = new HashMap<>();
+        connectorPayload.put("name", "products-connector");
+        connectorPayload.put("config", config);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(connectorPayload, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(kafkaConnectUri, HttpMethod.POST, entity, String.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            System.out.println("Connector created successfully");
+            return response.getStatusCode() + " Connector created successfully";
+        }
+        System.out.println("Failed to create connector: " + response.getBody());
+        return "Failed to create connector: " + response.getBody();
+    }
+}
---
+++ src/main/java/io/fusion/air/microservice/adapters/controllers/KafkaCDCController.java
@@ -0,0 +1,110 @@
+/**
+ * (C) Copyright 2023 Araf Karsh Hamid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fusion.air.microservice.adapters.controllers;
+
+import io.fusion.air.microservice.adapters.cdc.KafkaCDCPostgreSQLService;
+import io.fusion.air.microservice.domain.models.core.StandardResponse;
+import io.fusion.air.microservice.server.controllers.AbstractController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.io.IOException;
+
+/**
+ * Kafka SSE (Server Sent Event) Controller
+ *
+ * @author: Araf Karsh Hamid
+ * @version:
+ * @date:
+ */
+@CrossOrigin
+@Configuration
+@RestController
+// "/ms-cache/api/v1"
+@RequestMapping("${service.api.path}/kafka/cdc")
+@RequestScope
+@Tag(name = "Kafka CDC Controller", description = "Connect DB & Stream data to Topics (io.f.a.m.adapters.controllers.KafkaCDCController)")
+public class KafkaCDCController extends AbstractController {
+
+    @Autowired
+    private KafkaCDCPostgreSQLService kafkaDBConnectService;
+
+
+    @Operation(summary = "List All Connectors Registered with Kafka Connect")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Retrieved All Connectors registered!",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400",
+                    description = "Unable to retrieve all the Connectors",
+                    content = @Content)
+    })
+    @GetMapping(path = "/")
+    public ResponseEntity<StandardResponse> getAllConnectors() throws IOException {
+        String[] response = kafkaDBConnectService.listAllConnectors();
+        StandardResponse stdResponse = null;
+        if(response != null && response.length > 0) {
+            stdResponse= createSuccessResponse("Connect List Retrieved = "+response.length);
+            stdResponse.setPayload(response);
+        } else {
+            stdResponse= createSuccessResponse("No Connectors Found!");
+
+        }
+        return ResponseEntity.ok(stdResponse);
+    }
+
+    @Operation(summary = "Delete Connector Registered with Kafka Connect")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Deleted the registered Connector!",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400",
+                    description = "Unable to delete the registered Connector",
+                    content = @Content)
+    })
+    @DeleteMapping(path = "/{connectorName}")
+    public ResponseEntity<StandardResponse> deleteConnector(@PathVariable("connectorName") String _connectorName) throws IOException {
+        String response = kafkaDBConnectService.deleteConnector(_connectorName);
+        StandardResponse stdResponse = createSuccessResponse("Connect List Retrieved!");
+        stdResponse.setPayload(response);
+        return ResponseEntity.ok(stdResponse);
+    }
+
+    @Operation(summary = "Setup PostgreSQL for Streaming Product data to Topic")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Product Table setup for Streaming!",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400",
+                    description = "Unable to setup Product Table for Streaming",
+                    content = @Content)
+    })
+    @PostMapping(path = "/product")
+    public ResponseEntity<StandardResponse> dbProductSetup() throws IOException {
+        String response = kafkaDBConnectService.createPostgresConnectorForProduct();
+        StandardResponse stdResponse = createSuccessResponse("PostgreSQL Configured for Product Table");
+        stdResponse.setPayload(response);
+        return ResponseEntity.ok(stdResponse);
+    }
+}
